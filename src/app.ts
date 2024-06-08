37b7c0ece5b6a285fcfb8d1e1923ee0dc9631c91
riaz9191 <riazahammed9191@gmail.com>
2024-06-08 11:05:21 +0600

    template

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { ProductRoutes } from './module/product/product.route';
-import { OrderRoutes } from './module/order/order.route';
 
 const app = express();
 
@@ -10,7 +9,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/products', ProductRoutes);
-app.use('/api/orders', OrderRoutes);
 app.use((req: Request, res: Response, next: any) => {
   res.status(404).json({
     success: false,
--- src/module/product/product.route.ts
+++ src/module/product/product.route.ts
@@ -4,7 +4,7 @@ import { ProductControllers } from './product.controller';
 const router = express.Router();
 
 router.post('/', ProductControllers.createProduct);
-router.get('/', ProductControllers.getAllProducts);  // Updated to handle searchTerm
+router.get('/', ProductControllers.getAllProducts); 
 router.get('/:productId', ProductControllers.getProductById);
 router.put('/:productId', ProductControllers.updateProduct);
 router.delete('/:productId', ProductControllers.deleteProduct);
